64cd2a9fbe39a6ca594ae06fdadf81f84125a27a
Meagan Hollingsworth <mhollingsworth@google.com>
2024-02-02 18:20:54 -0500

    fixed the server.js file so i can open the server to listen to port 3000

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+const { db } = require("./server/models");
 const app = require("./server/app");
 
 const PORT = process.env.PORT || 3000;
 
 const init = async () => {
-    try {
-        await db.sync();
+  try {
+    await db.sync();
 
-        app.listen(PORT, () => {
-            console.log(`Server listening on port ${PORT}`);
-        })
-    } catch (error) {
-        console.error('Error starting server:', error)
+    app.listen(PORT, () => {
+        console.log(`Server listening at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting server:', error)
     }
 };
 
--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 //imported libraries
 const express = require('express');
 const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
+
 
 //express app
 const app = express()
 app.use(bodyParser.json())
 
+
+module.exports = app;
 
 
 
