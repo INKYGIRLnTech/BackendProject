a9515d3f6cc81f265186b2da17b44a8261591ad7
Meagan Hollingsworth <mhollingsworth@google.com>
2024-02-02 18:52:35 -0500

    added the error handling middleware to the app.js

--- server/app.js
+++ server/app.js
@@ -8,6 +8,15 @@ const app = express()
 app.use(bodyParser.json())
 
 
+
+// error handling middleware
+app.use((error, req, res, next) => {
+    console.error('SERVER ERROR: ', error);
+    if(res.statusCode < 400) res.status(500);
+    res.send({error: error.message, name: error.name, message: error.message, table: error.table});
+});
+
+
 module.exports = app;
 
 
