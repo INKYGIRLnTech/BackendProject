27115f8c900230cadad4c8b4d541b4a6383d9c01
Meagan Hollingsworth <mhollingsworth@google.com>
2024-02-07 13:16:26 -0500

    added the update and delete routes to products.js and fixed some bugs in the create and read routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,21 +2,52 @@ const express = require("express");
 const router = express.Router();
 const { Product } = require("../models");
 
-// create new product
-router.post('/products', async (req, res) => {
+// create/add new product
+router.post("/", async (req, res, next) => {
     try{
-        const {name, description, price} = req.body;
-        const newProduct = await Product.create({name, description, price});
-        res.status(201).json(newProduct);
-    }catch(error){
-        res.status(400).json({error: error.message})
+        const product = await Product.create(req.body)
+        res.json(product)
+    } catch(err) {
+        next(err)
     }
 });
 
-// read all products
-router.get('/products', async (req, res) => {
-    const products = await Product.findAll();
-    res.json(products);
-})
+
+// read/find one products
+router.get("/:id", async (req, res, next) => {
+    try{
+        const id = req.params.id
+        const product = await Product.findByPk(id)
+        res.json(product)
+    } catch(err) {
+        next(err)
+    }
+});
+
+//update products by ID
+router.put("/:id", async (req, res, next) => {
+    try{
+        const id = req.params.id;
+        const updatedProduct = await Product.update(req.body, {
+            where: {id: id}
+        })
+        res.json(updatedProduct)
+    } catch(err) {
+        next(err)
+    }
+});
+
+//delete a product
+router.delete("/:id", async (req, res, next) => {
+    try{
+        const deleted = await Product.findByPk(req.params.id);
+        await Product.destroy({
+            where: {id: req.params.id}
+        })
+        res.json(deleted)
+    } catch(err){
+        next(err)
+    }
+});
 
 module.exports = router;
